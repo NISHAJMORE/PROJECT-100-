0a78331c0bb10eb0cc2089df8955347d3a5f6d8c
NISHAJMORE <120033928+NISHAJMORE@users.noreply.github.com>
2024-02-27 13:56:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+  
+  setTimeout(function()
+    {
+        img_id = "selfie1";
+        take_snapshot();
+        speak_data = "Taking your next selfie in 10 seconds";
+        var utterThis = new SpeechRecognitionUtterance(speak_data);
+        SpeechSynthesis.speak(utterThis);
+    },
+   5000);
+
+   function take_snapshot()
+   {
+    console.log(img_id);
+
+    WebCam.snap(function(data_uri){
+      if(img_id=="selfie1"){
+        document.getElementById("result1").innerHTML ='<img id="selfie1" src=" '+data_uri+'"/>';
+      }
+      if(img_id=="selfie2"){
+        document.getElementById("result2").innerHTML ='<img id="selfie2" src=" '+data_uri+'"/>';
+      }
+      if(img_id=="selfie3"){
+        document.getElementById("result3").innerHTML ='<img id="selfie2" src=" '+data_uri+'"/>';
+      }
+    });
+   }
